9f83bebc58818d374fa5fb288f3bcbd8f2cbd1ec
X-San <70487315+XWasHere@users.noreply.github.com>
2020-10-14 12:13:32 -0400

    Update CONSTANTS.cs

--- src/CONSTANTS.cs
+++ src/CONSTANTS.cs
@@ -1,5 +1,5 @@
 namespace Amino {
-class constants {
-  const string API_ENDPOINT = @"placeholder";
-}
+  class constants {
+    const string API_ENDPOINT = @"http://service.narvii.com/api/v1";
+  }
 }
